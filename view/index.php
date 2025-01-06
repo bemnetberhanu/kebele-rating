ede7f8d5fa105e12038bd3c83b8c64675b26c981
bemnetberhanu <bemnetberhanu98@gmail.com>
2025-01-06 12:41:30 +0300

    initial

---
+++ view/index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Document</title>
+</head>
+<body >
+     <div class="container">
+            <nav>
+                <img src="../image/ad.jpeg" class="logo">
+                <ul>
+                    <li><a href="#subc">home</a></li>
+                    <li><a href="#about">about</a></li>
+                    <li><a href="#services">service</a></li>
+                    <li><a href="#contact">contact</a></li>
+                    <li><a href="#logout">logout</a></li>
+                    
+                </ul>
+            </nav>
+     </div>
+    <div class="header-text">
+   <h1>    WELCOME TO ADISS ABEBA ADMINSTRATION</h1>
+   <h3>WELCOME TO THE RATING SYSTEM</h3>
+     <h2>choose your subcity</h2>
+     </div>
+     <div id="subc">
+        <div class="subci">
+     <a href="addisketema.php"><img src="../image/adisketema.jpeg" alt="photo "></a><br>
+     <h3>ADDIS KETEMA</h3>
+     </div>
+    <div class="subci">
+     <a href="akakikalit.php"><img src="../image/akaki.jpeg" alt=" "></a><br>
+     <H3>AKAKI KALITI</H3>
+</div>
+     <div class="subci">
+     <a href="aradasubcity.php"><img src="../image/arada.jpeg" alt=" "> </a><br>
+     <H3>arada</H3>
+     </div>     
+     <div class="subci">
+     <a href="bole.php"><img src="../image/bole.jpeg" alt=" "></a> <br>   
+     <H3>bole</H3>
+     </div>
+     <div class="subci">
+     <a href="gullele.php"><img src="../image/gulele.jpeg" alt=" "></a><br>
+     <H3>gullele</H3>
+     </div>
+     <div class="subci">
+     <a href="kirkos.php"><img src="../image/kirkos.jpeg" alt=" "></a><br>
+     <H3>kirkos</H3>
+     </div>
+     <div class="subci">
+     <a href="kolfekeranyo.php"><img src="../image/kolfe.jpeg" alt=" "></a><br>
+     <H3>kolfe keranio</H3>
+     </div>
+     <div class="subci">
+     <a href="lideta.php"><img src="../image/lideta.jpeg" alt=" "></a><br>
+     <H3>lideta</H3>
+     </div>
+     <div class="subci">
+     <a href="nifassilk.php"><img src="../image/nefas.jpeg" alt=" "></a><br>
+     <H3>nifas silk lafto</H3>
+     </div>
+     <div class="subci">
+     <a href="yeka.php"><img src="../image/yeka.jpeg" alt=" "></a><br>
+     <H3>yeka</H3>
+     </div>
+     <div class="subci">
+     <a href="lemikura.php"><img src="../image/lemi.jpeg" alt=" "></a><br>
+      <H3>lemi kura</H3>
+     </div>
+    </div>
+
+
+    <div id="about">
+     <div class="subc">
+        <h1>about this system</h1><br><br>
+        <h4>this system is created for citizenes in order to help
+            them to rate the service they get from the subcity specficaly the kebele 
+        </h4>
+      </div>
+    </div>
+
+    <!-- service-->
+
+    <!--contact us-->
+<div id="contact">
+    <div class="subc">
+    <h2>Mayor's Office</h2>
+      <label name=" location">
+         <h4>Addis Abeba ,Ethiopia</h4>
+         <h4>phone: </h4>
+          <h4>01181268115</h4>
+        </div>
+     </div>
+</body>
+</html>
